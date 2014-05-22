87b520638139d71ed9554e65bee79842cdf0831c
pere5 <per.eriksson@endian.se>
2014-05-22 15:19:48 +0200

    Adding some logging for nice colors (same as Spring boot is using )

--- src/main/java/openapi/Application.java
+++ src/main/java/openapi/Application.java
@@ -10,21 +10,24 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.Arrays;
+import org.apache.log4j.Logger;
 
 @EnableAutoConfiguration
 @Configuration
 @ComponentScan
 public class Application {
 
+    private static Logger logger = Logger.getLogger(Application.class);
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        logger.info("Let's inspect the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-            System.out.println(beanName);
+            logger.info(beanName);
         }
     }
 
