50447151ce5fc1e272f97a256fedfc5d35b786b1
pere5 <per.eriksson@endian.se>
2014-05-22 15:02:49 +0200

    Making JSTL work with Spring Model

--- src/main/java/openapi/GreetingController.java
+++ src/main/java/openapi/GreetingController.java
@@ -1,8 +1,11 @@
 package openapi;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -12,12 +15,15 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "greeting.json", method = RequestMethod.GET)
-    public @ResponseBody Greeting greetingJSON(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public @ResponseBody Greeting greetingJSON(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
     @RequestMapping(value = "greeting.htm", method = RequestMethod.GET)
-    public String greetingHTM(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public String greetingHTM(Model model, @RequestParam(value="name", required=false, defaultValue="Dood!") String name) {
+        Map<String, Object> attributesMap = new HashMap<>();
+        attributesMap.put("name", name);
+        model.addAllAttributes(attributesMap);
         return "greeting";
     }
 }
--- src/main/webapp/views/greeting.jsp
+++ src/main/webapp/views/greeting.jsp
@@ -1,8 +1,9 @@
+<%@ page language="java" contentType="text/html;charset=ISO-8859-1"%>
 <html>
 <body>
 <h1>Hello World</h1>
 
-<h2>Hi doods!</h2>
+<h2>Hi ${name}!</h2>
 
 </body>
 </html>
