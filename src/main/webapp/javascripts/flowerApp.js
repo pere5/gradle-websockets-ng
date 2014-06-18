e9f1116279e7d364d47b2979cb0fbb4daa92bcfa
pere5 <per.eriksson@endian.se>
2014-06-18 18:40:21 +0200

    Don't want people to see all the goodies from start.

--- src/main/webapp/javascripts/flowerApp.js
+++ src/main/webapp/javascripts/flowerApp.js
@@ -12,7 +12,7 @@ var controllers = angular.module('controllers', []);
 flowerApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
         when('/flowerBed', {
-            templateUrl: '/views/flowerBed.html',
+            templateUrl: '/views/nothing.html',
             controller: 'flowerbedController'
         }).
         otherwise({
